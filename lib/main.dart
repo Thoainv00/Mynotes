30deb237913dc7c8111922ee12bfff482529996e
thoainv <thoai94no1@gmail.com>
2024-01-12 07:00:46 +0700

    Step_2

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,13 @@
 // import 'dart:js';
 
 // import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:mynotes/firebase_options.dart';
 import 'package:mynotes/views/login_view.dart';
 import 'package:mynotes/views/register_view.dart';
+import 'package:mynotes/views/verify_view.dart';
 // import 'package:mynotes/views/register_view.dart';
 // import 'package:mynotes/views/login_view.dart';
 // import 'package:firebase_auth/firebase_auth.dart';
@@ -40,15 +42,17 @@ class HomePage extends StatelessWidget {
       builder: (context, snapshot) {
         switch (snapshot.connectionState) {
           case ConnectionState.done:
-            // final user = FirebaseAuth.instance.currentUser;
-            // print(user);
-            // if (user?.emailVerified ?? false) {
-            //   print('you are a verified email');
-            //   return const Text('Done');
-            // } else {
-            //   return const VerifyEmailView();
-            // }
-            return const LoginView();
+            final user = FirebaseAuth.instance.currentUser;
+            if (user != null) {
+              if (user.emailVerified) {
+                print('Email is verified');
+              } else {
+                return const VerifyEmailView();
+              }
+            } else {
+              return const LoginView();
+            }
+            return const Text('Done');
           default:
             return const CircularProgressIndicator();
         }
